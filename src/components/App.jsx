51cf10de88f1edffecc0d1e487a92cbb15995bf0
burkegg <burkegreen@gmail.com>
2018-06-27 20:57:56 -0700

    In progress: Send handlers to children and get info back.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,39 @@
 
+
+// We are going to do a help desk request first thing.
+// Looking to pass the click handler down,
+// we don't understand the ideas involved,
+// but we know that passing a handler down is possible.
+// Want to figure out how to bind the click handler to 
+// a particular element and transmit attributes about hte leement back.
+//(for isntane, we want to get the clicked element's video ID)
+//and also how/if it changes the App's state property
+
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentVideo: null,
+      allVideos: []
+    };
   }
+  
+  getCurrentVideoInfo() {
+    //onClick=this.
+    // when we click on a video get the clicked video's id.
+    // set this.state.currentVideo to the id received by click.
+    
+  }
+  
+  handleClick() {
+    this.onClick.bind(this),
+
+    this.setState({
+      // what did we click on?
+      currentVideo: 'FIX_ME'
+    });
+  }
+  
   render() {
     return ( 
       <div>
@@ -17,7 +48,9 @@ class App extends React.Component {
           </div>
           <div className="col-md-5">
             {/*<div><h5><em>videoList</em> view goes here</h5></div>*/}
-            <VideoList videos={window.exampleVideoData}/>
+            <VideoList 
+              videos={window.exampleVideoData} 
+            />
             {console.log(window.exampleVideoData)}
           </div>
         </div>
